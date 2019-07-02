cdd591eb15c5b2692adec2de4633b03958d96af0
prakashdivvela <52447437+prakashdivvela@users.noreply.github.com>
2019-07-02 15:39:23 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,11 @@
+b=str(input())
+vow=["a","e","i","o","u"]
+con=["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
+if b in vow:
+    print("Vowel")
+elif b in con:
+    print("Consonant")
+else:
+  print("invalid")
+
+  
