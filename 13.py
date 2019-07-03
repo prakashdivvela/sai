e257cadaee9c340ce0e9ab32525cdb6291fae490
prakashdivvela <52447437+prakashdivvela@users.noreply.github.com>
2019-07-03 12:25:23 +0530

    Update 13.py

--- 13.py
+++ 13.py
@@ -1,11 +1,10 @@
 n=int(input())
-n>0
-for i in range(2,n):
-  if(n%i==0):
-    print("no")
-    break
+if n>1:
+  for i in range(2,n):
+     if(n%i==0):
+       print("no")
+       break
   else:
     print("yes")
-    break
 else:
   print("no")
