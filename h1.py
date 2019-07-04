0adab2f263499de84ee7fa063d73c6f4b136d989
prakashdivvela <52447437+prakashdivvela@users.noreply.github.com>
2019-07-04 18:30:59 +0530

    Create h1.py

---
+++ h1.py
@@ -0,0 +1,12 @@
+p=int(input())
+q=list(map(int,input().split()))
+q.sort()
+m=[]
+for i in range(len(q)-1):
+  if q[i]==q[i+1]:
+    m.append(q[i])
+if m:
+  for x in set(m):
+    print(x,end=" " )
+else:
+  print("unique")
