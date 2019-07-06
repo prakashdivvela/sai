bdee7709abb809f591b3790f7b0c1629623cd06f
prakashdivvela <52447437+prakashdivvela@users.noreply.github.com>
2019-07-06 11:55:32 +0530

    Update p32.py

--- p32.py
+++ p32.py
@@ -1,7 +1,7 @@
-p=int(input())
-s=list(map(int,input().split()))
-x=s[0:p:2]
-y=s[1:p:2]
+q=int(input())
+r=list(map(int,input().split()))
+x=r[0:p:2]
+y=r[1:p:2]
 if sum(x)>=sum(y):
   print(sum(x))
 else:
