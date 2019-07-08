d2eba72d551d8e72b09f0f11e369f63616eff258
prakashdivvela <52447437+prakashdivvela@users.noreply.github.com>
2019-07-08 18:28:57 +0530

    Update p22.py

--- p22.py
+++ p22.py
@@ -1,5 +1,5 @@
-p,q=map(int,input().split())
+a,b=map(int,input().split())
 r=list(map(int,input().split()))
-for i in range(q):
+for i in range(b):
   s,t=map(int,input().split())
 print(sum(r[s-1:t]))
