bef4de86faf5bfbb568ef9fc71becd83c6e4618d
prakashdivvela <52447437+prakashdivvela@users.noreply.github.com>
2019-07-26 19:07:29 +0530

    Update h18.py

--- h18.py
+++ h18.py
@@ -1,7 +1,7 @@
-n=input()
-l=list(map(int,input().split()))
+n=int(input())
+b=list(map(int,input().split()))
 for i in range(0,n):
   for j in range(i+1,n):
     for k in range(j+1,n):
-      if(l[i]+l[j]==l[k]):
-        print(l[i],l[j],l[k])
+      if(b[i]+b[j]==b[k]):
+        print(b[i],b[j],b[k])
