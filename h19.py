e035d839f02de29b8086c3f67f648d3949f0d2e3
prakashdivvela <52447437+prakashdivvela@users.noreply.github.com>
2019-07-26 19:10:58 +0530

    Update h19.py

--- h19.py
+++ h19.py
@@ -1,6 +1,6 @@
 n=int(input())
-l=list(map(int,input().split()))
+b=list(map(int,input().split()))
 for i in range(n):
   for j in range(i+1,n):
-      if(l[i]+l[j]==0):
-        print(l[i],l[j])
+      if(b[i]+b[j]==0):
+        print(b[i],b[j])
