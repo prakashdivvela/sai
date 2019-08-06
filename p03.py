8bbf8ba0059ac30aadf8b148589cb05c275ea410
prakashdivvela <52447437+prakashdivvela@users.noreply.github.com>
2019-08-06 22:04:41 +0530

    Update p03.py

--- p03.py
+++ p03.py
@@ -1,7 +1,7 @@
 x,y=map(list,input().split())
 c=abs(len(x)-len(y))
-for i in range(len(a)):
-    if len(y)==1 and y[i] in a:
+for i in range(len(x)):
+    if len(y)==1 and y[i] in x:
         break
     if x[i]!=y[i]:
         c+=1
